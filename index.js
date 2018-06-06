006ea0855fc1fff21e8dfbf151fc4e0625e5b6d4
outbounder <obiwonn@gmail.com>
2018-06-06 21:29:51 +0300

    morphdom complete

--- index.js
+++ index.js
@@ -1,53 +1,51 @@
-const { h, render, Component } = require('preact')
-const hyperx = require('hyperx')
-const Empty = () => null
-const hx = hyperx((tag, props, kids) => {
-  if (!props.ref) {
-    let dataProps = {}
-    let handlers = {}
-    for (let key in props) {
-      if (typeof props[key] === 'object') {
-        dataProps[key] = props[key]
-        delete props[key]
-      }
-      if (typeof props[key] === 'function') {
-        handlers[key] = props[key]
-        delete props[key]
-      }
-    }
-    props.ref = function (el) {
-      if (el) {
-        for(let key in dataProps) {
-          el[key] = dataProps[key]
-        }
-        for (let key in handlers) {
-          el.addEventListener(key, handlers[key])
-        }
-      }
-    }
+module.exports = function (plasma, dna) {
+  if (!dna.engine) {
+    Object.assign(module.exports, require('./lib/morphdom'))
   }
-  return h(tag, props, kids)
-})
+  require('domready')(function () {
+    plasma.emit('oval-ready')
+  })
+}
+
+module.exports.html = function (component) {
+  throw new Error('organic-oval is missing html string literal function')
+}
+
+module.exports.render = function (component) {
+  throw new Error('organic-oval is missing render function')
+}
+
+module.exports.defineComponent = function (tagName, Class) {
+  customElements.define(tagName, Class)
+}
 
 module.exports.WebComponent = class WebComponent extends HTMLElement {
   static define (tagName) {
-    customElements.define(tagName, this)
+    module.exports.defineComponent(tagName, this)
   }
   constructor () {
     super()
-    this.html = hx
-    this.attachShadow({mode: 'open'})
+    this.html = module.exports.html(this)
+    this.render = module.exports.render
+    this.state = {}
+    this.refs = {} // populated during rendering
+    this.slots = {} // populated during rendering
+    this.kids = null // populated during rendering
   }
-  render () {}
+  template () {}
   update () {
     this.emit('update')
-    this._root = render(this.render(), this.shadowRoot, this._root)
+    this.render(this)
     this.emit('updated')
+  }
+  shouldUpdate () {
+    this.update()
   }
   connectedCallback () {
     this.emit('mount')
     this.update()
     this.emit('mounted')
+    this.mounted = true
   }
   attributeChangedCallback () {
     this.update()
@@ -57,12 +55,15 @@ module.exports.WebComponent = class WebComponent extends HTMLElement {
     // TODO? 
     this.emit('unmounted')
   }
+  
+  /* helper methods */
   on (eventName, eventHandler) {
     this.addEventListener(eventName, eventHandler)
   }
   emit (eventName, eventData) {
     let e = new CustomEvent(eventName, {
-      detail: eventData
+      detail: eventData,
+      bubbles: false
     })
     this.dispatchEvent(e)
   }
